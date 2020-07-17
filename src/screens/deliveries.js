fae5ab3c7aaa8402c73a2d4a8347c2f4d42539a1
Andres <asebvila@gmail.com>
2020-07-16 23:30:28 -0300

    layout screen of deliveries

--- src/screens/deliveries.js
+++ src/screens/deliveries.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, ActivityIndicator, TouchableOpacity, ScrollView } from 'react-native';
-import { Row, Rows, Table } from 'react-native-table-component';
+import { StyleSheet, Text, View, ScrollView, RefreshControl } from 'react-native';
+import { Row, Table } from 'react-native-table-component';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useSelector, useDispatch } from 'react-redux';
+import ViewIsLoading from '../components/layout/viewIsLoading';
 import { invokeGetDeliveries } from '../redux/actions'
 import { formatDate } from '../helpers/dateHelpers';
 
@@ -9,7 +11,7 @@ export default function Deliveries() {
   const userToken = useSelector(store => store.auth.token);
   const deliveries = useSelector(store => store.deliveries.deliveries);
   const isLoading = useSelector(store => store.deliveries.isLoading)
-  const tableHead = ["cliente", "proxima entrega", "ultima entrega", "articulo"];
+  const tableHead = ["Cliente", "Proxima entrega", "Ultima entrega", "Articulo"];
   
   const dispatch = useDispatch();
 
@@ -17,71 +19,108 @@ export default function Deliveries() {
     invokeGetDeliveries(dispatch, userToken)
   }, [])
 
+  function createlogs() {
+    let arrayLogs = [];
+    for(let i = 0 ; i < 50 ; i++){
+      arrayLogs.push({
+        client: "alguiennnnnnnnnn",
+        nextDelivery: "19/07/20",
+        lastDelivery: "19/07/20",
+        article: "Nutra nuggets verde 15kg"
+      })
+    }
+    return arrayLogs;
+  }
+
   function createTable() {
+    //const arrayLogs = createlogs();
     const deliveriesTable = deliveries.map((log, key) => {
       const nextDelivery = new Date(log.nextDelivery._seconds * 1000)
       const lastDelivery = new Date(log.lastDelivery._seconds * 1000)
       return [
-        <TouchableOpacity style={styles.button}>
-          <Text>{log.client}</Text>
-        </TouchableOpacity>,
-        formatDate(nextDelivery),
-        formatDate(lastDelivery),
-        log.article
+        <Text style={styles.cellClient} numberOfLines={1}>{log.client}</Text>,
+        <Text style={styles.cell} numberOfLines={1}>{formatDate(nextDelivery)}</Text>,
+        <Text style={styles.cell} numberOfLines={1}>{formatDate(lastDelivery)}</Text>,
+        <Text style={styles.cell} numberOfLines={1}>{log.article}</Text>
       ]
     })
 
     return deliveriesTable
   }
 
-  function refreshLogs(){
-    console.warn('refresh logs')
+  function handleRefresh(){
+    invokeGetDeliveries(dispatch, userToken)
   }
 
   return (
     <View style={styles.container}>
-      <Button title="Recargar" onPress={refreshLogs} />
-      <Text style={styles.text}>Precione el nombre del cliente para elimiar el registro</Text>
-      {
-        isLoading ? <ActivityIndicator size="large" color="#0000ff" /> : null
-      }
-      <View style={styles.logView}>
-        <Table>
-          <Row data={tableHead} style={styles.head} textStyle={styles.text} />
-        </Table>
-        <ScrollView>
-          <Table borderStyle={{ /*borderWidth: 2,*/ borderColor: 'transparent' }}>
-            <Rows data={createTable()} textStyle={styles.text} />
+      <View style={styles.logView} >
+        <LinearGradient style={styles.headRow} colors={['#2b8ff3', '#1cacdc']}> 
+          <Table>
+            <Row opacityPress={1} data={tableHead} textStyle={styles.headText} />
           </Table>
-        </ScrollView>
+        </LinearGradient>
+        {
+          deliveries ?
+            <ScrollView refreshControl={
+              <RefreshControl 
+              refreshing={isLoading} 
+              colors={['#1885f2', '#2b8ff3', '#1cacdc']}
+              size='large'
+              onRefresh={handleRefresh} 
+              />}
+            >
+              <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                {
+                  createTable().map((row, key) => (
+                    <Row key={key} data={row} onRowPress={() => console.warn('eeee')} />
+                    ))
+                }
+              </Table>
+            </ScrollView>
+          :
+            <View style={styles.containerLoader}>
+              <ViewIsLoading />
+            </View>
+        }
       </View>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    paddingTop: 30,
+    paddingHorizontal: 5,
     backgroundColor: '#fff',
   },
-  head: {
-    height: 40,
-    backgroundColor: '#b2cbe3'
-  },
-  text: {
-    margin: 6
-  },
-  button: {
-    backgroundColor: "#2196f3",
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30,
-    padding: 2
-  },
   logView: {
-    marginBottom: 119
+    marginBottom: 98
+  },
+  headRow: {
+    height: 55,
+    marginTop: 40,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  headText: {
+    marginBottom: 6,
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#FFCC3D',
+    fontWeight: 'bold'
+  },
+  cell: {
+    marginHorizontal: 3,
+    marginVertical: 3
+  },
+  cellClient: {
+    marginHorizontal: 3,
+    marginVertical: 8,
+    color: '#1885f2',
+    fontWeight: 'bold'
+  },
+  containerLoader: {
+    paddingTop: 50
   }
 });
