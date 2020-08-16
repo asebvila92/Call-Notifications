4e61a5522c45c923f095fb22250e7c17eed6dbe2
Andres <asebvila@gmail.com>
2020-08-16 20:24:14 -0300

    migrate server to cloud functions

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,14 +1,16 @@
+import { ROUTE_DEV, ROUTE_PRODUCTION } from './routesApi'; //this file is not commited
+
 export const IS_DEV = __DEV__;
 export const IS_PRODUCTION = !IS_DEV;
 
 export const DEV_ENV = {
-  API_DOMAIN: 'http://192.168.1.16:3000',
+  API_DOMAIN: ROUTE_DEV,
 }
 
 //We will build the project with development values while we dont have API in production, for that 
 //reason the values are the same in both environments
 export const PRODUCTION_ENV = {
-  API_DOMAIN: 'https://server-notifications.herokuapp.com', 
+  API_DOMAIN: ROUTE_PRODUCTION, 
 }
 
 export const ENVIRONMENT = IS_DEV ? DEV_ENV : PRODUCTION_ENV
